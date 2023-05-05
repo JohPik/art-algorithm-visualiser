bf2579640dbefcc61f33e89238978db81d5c32f4
JohPikEPE <johan.piquet@epequip.com>
2023-05-05 14:31:19 +1000

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 // Components
-import Header from './components/Header'
-import Wall from './components/Wall'
-import Form from './components/Form'
+import Header from './components/Header';
+import Wall from './components/Wall';
+import Form from './components/Form';
 
 export default function App() {
   return (
     <>
       <Header />
-      <div className="main"> 
-        <Wall/>
-        <Form/>
+      <div className='main test'>
+        <Wall />
+        <Form />
       </div>
     </>
   );
